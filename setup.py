f149c4707d7932674119f59674aa8dc00bfd43d5
Alan <alanmunguiacerda@hotmail.com>
2020-08-16 10:36:57 -0500

    Add missing comma

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Environment :: Web Environment',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ]
 )
