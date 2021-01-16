75dab4740f6346f1185ef1db8675119ceb54995e
Alan <alanmunguiacerda@hotmail.com>
2021-01-16 12:23:34 -0600

    Fix example

--- examples/__init__.py
+++ examples/__init__.py
@@ -5,6 +5,6 @@ from flask_restforms import FlaskRestForms
 app = flask.Flask(__name__)
 flask_restforms = FlaskRestForms(app)
 
-@app.route('/', methods=['GET', 'POST', 'DELETE'])
+@app.route('/', methods=['GET', 'POST', 'DELETE', 'PATCH'])
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html', method=flask.request.method)
